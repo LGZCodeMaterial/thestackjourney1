057eed25fccc754dca58c526751f40778cd44261
unknown <GZliew>
2019-02-23 23:40:45 +0800

    main and faqs page added

--- src/App.js
+++ src/App.js
@@ -1,44 +1,32 @@
-import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
+import React from "react";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import "./App.css";
+import Routes from "./routes";
+import { lightBlue, grey } from "@material-ui/core/colors";
+import SpeedDailBtn from "./components/Button/SpeedDailBtn";
 
-const TestCom = () => {
-    const [name, setName] = useState("billy");
-
-    const changeName = e => {
-        setName(e.target.value);
-    };
-
-    useEffect(() => {
-        document.title = name;
-    });
-
-    return (
-        <div>
-            {name}
-            <input type="text" value={name} onChange={changeName} />
-        </div>
-    );
-};
+const theme = createMuiTheme({
+    palatte: {
+        secondary: {
+            main: lightBlue[900]
+        },
+        primary: {
+            main: grey[50]
+        }
+    },
+    typography: {
+        fontFamily: ['"Lato"', "sans-serif"].join(","),
+        useNextVariants: true,
+    }
+});
 
 const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <TestCom />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload....
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div>
+            <MuiThemeProvider theme={theme}>
+                <Routes />
+                <SpeedDailBtn />
+            </MuiThemeProvider>
         </div>
     );
 };
