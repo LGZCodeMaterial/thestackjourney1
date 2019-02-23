8f80340254b83729e2a325fbf53f643b880dd571
unknown <GZliew>
2019-02-23 13:39:02 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
-  render() {
+const TestCom = () => {
+    const [name, setName] = useState("billy");
+
+    const changeName = e => {
+        setName(e.target.value);
+    };
+
+    useEffect(() => {
+        document.title = name;
+    });
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+            {name}
+            <input type="text" value={name} onChange={changeName} />
+        </div>
     );
-  }
-}
+};
+
+const App = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <TestCom />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload....
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+};
 
 export default App;
